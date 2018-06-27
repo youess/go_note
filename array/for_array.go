33708ee852cb78dbef3d142ab90339d3d1a075bf
denglei <denglei@qutoutiao.net>
2018-06-27 20:38:06 +0800

    add example for two dimension array range

--- array/for_array.go
+++ array/for_array.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 
-	nums := []int{10, 20, 30, 40}
+	numA := []int{10, 20, 30, 40}
 
-	for idx, e := range nums {
+	for idx, e := range numA {
+		fmt.Printf("idx: %d, val: %d\n", idx, e)
+	}
+
+	numB := [][2]int{{1, 2}, {3, 4}, {4, 5}}
+	fmt.Println("")
+	for idx, e := range numB {
 		fmt.Printf("idx: %d, val: %d\n", idx, e)
 	}
 }
