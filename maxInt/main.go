04237cbe3c7da55f7b9b7778ff0a1f7b6f746d35
denis <denis_lton@hotmail.com>
2018-07-23 15:40:17 +0800

    add max integer constant expression

---
+++ maxInt/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+//import "reflect"
+
+const BitsPerWord = 32 << (^uint(0) >> 63)  // either 32 or 64
+
+const (
+    MaxInt  = 1<<(BitsPerWord-1) - 1 // either 1<<31 - 1 or 1<<63 - 1
+    MinInt  = -MaxInt - 1            // either -1 << 31 or -1 << 63
+    MaxUint = 1<<BitsPerWord - 1     // either 1<<32 - 1 or 1<<64 - 1
+)
+
+func main() {
+
+    fmt.Printf("uint(0): %d\n", uint(0))
+    fmt.Printf("^uint(0): %d\n", ^uint(0))
+    fmt.Printf("bits per word : %d\n", BitsPerWord)
+    fmt.Printf("max int: %d\n", MaxInt)
+    fmt.Printf("min int: %d\n", MinInt)
+    fmt.Printf("max uint: %d\n", uint64(MaxUint))
+
+}
